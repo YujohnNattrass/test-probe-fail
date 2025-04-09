d58b1bcca91b7d3cf80c0f34ec53d5ad09916512
YujohnNattrass <46704329+YujohnNattrass@users.noreply.github.com>
2025-04-08 19:19:49 -0700

    Update index.ts

--- src/mastra/index.ts
+++ src/mastra/index.ts
@@ -3,6 +3,13 @@ import { Mastra } from '@mastra/core/mastra';
 import { createLogger } from '@mastra/core/logger';
 import { weatherWorkflow } from './workflows';
 import { weatherAgent } from './agents';
+import { DefaultStorage } from '@mastra/core/storage/libsql';
+
+export const defaultStorage = new DefaultStorage({
+  config: {
+    url: "file:mastra.db",
+  },
+});
 
 export const mastra = new Mastra({
   workflows: { weatherWorkflow },
@@ -11,4 +18,5 @@ export const mastra = new Mastra({
     name: 'Mastra',
     level: 'info',
   }),
+  storage: defaultStorage,
 });
